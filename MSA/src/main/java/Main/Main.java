82afc9c0dcd7e3260a4c4fa71591895f1fd56c56
Merlin33333 <89148716+Merlin33333@users.noreply.github.com>
2022-03-24 08:08:48 +0100

    Add files via upload

---
+++ MSA/src/main/java/Main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author vande
+ */
+public class Main {
+
+    
+    public void a(){
+        System.out.println("aAAAAAAAAAAAA");
+    }
+    
+}
+
